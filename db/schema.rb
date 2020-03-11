94d56c36c47d1c2d647a74652f92dff58c5a869e
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-11 12:49:46 +0900

    rename tools tool=>type
    取り出せないのが名前の問題かどうかをはっきりさせるため
    取り出せないので、リレーションの掛け方の問題かもしれない。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_013539) do
+ActiveRecord::Schema.define(version: 2020_03_11_024548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_013539) do
   end
 
   create_table "tools", force: :cascade do |t|
-    t.string "tool", null: false
+    t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
