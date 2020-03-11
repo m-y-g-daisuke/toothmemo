3a40c16308db8b6dc9084511c4fc32e81a7f588d
m-y-g-daisuke <disk.myg@gmail.com>
2020-03-11 19:19:13 +0900

    controller definitions and make pathes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_animal
   def index
+    @posts=@animal.posts
   end
 
   def show
+    @post=Post.find(paramas[:id])
   end
 
   def new
+    @post=@animal.posts.new
   end
-
+  
   def create
+    @post=@animal.posts.new(post_params)
+    if @post.save
+      redirect_to animal_path(@animal)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @post=@animal.posts.find(params[:id])
   end
 
   def update
+    @post=@animal.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to animal_path(@animal)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    post=@animal.posts.find(params[:id])
+    post.destroy
+    redirect_to animal_posts_path(@animal)
   end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:recorded_at, :content_g,:content_b, :memo, :tool, :image)
+  end
+
+  def set_animal
+    @animal=Animal.find(params[:animal_id])
+  end
+
 end
