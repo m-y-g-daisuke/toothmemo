3ea796dcf86c25b3e338c317e18aab6adf3ed06a
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-14 19:38:46 +0900

    before_actionの設定

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,9 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: [:show, :edit, :update]
   def index
   end
 
   def show
-    @animal=Animal.find(params[:id])
     @post=Post.where(animal_id:params[:id]).last
     @graph=Post.where(animal_id:params[:id]).last(14)
     breed=@animal.breed_id
@@ -25,11 +25,9 @@ class AnimalsController < ApplicationController
   end
   
   def edit
-    @animal=Animal.find(params[:id])
   end
   
   def update
-    @animal=Animal.find(params[:id])
     if @animal.update(animal_params)
       redirect_to animal_path(@animal)
     else
@@ -44,6 +42,10 @@ class AnimalsController < ApplicationController
 
   def animal_params
     params.require(:animal).permit(:name, :birth, :sex, :breed_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_animal
+    @animal=Animal.find(params[:id])
   end
 
 end
