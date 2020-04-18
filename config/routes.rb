41fba5ce411a2f48e86454fcb7be6e66452c2f9c
m-y-g-daisuke <disk.myg@gmail.com>
2020-04-18 19:34:40 +0900

    不要なコメントアウト の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "animals#index"
   resources :users,   only: [:edit, :update]
+  root "animals#index"
   resources :animals do
     resources :posts
   end
