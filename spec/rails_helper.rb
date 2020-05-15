0bab11d3820c7a678e463cb6a3d3163740e44066
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-15 16:49:13 +0900

    factorybotの記述の省略のための記述

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #factorybotのメソッド呼び出しの効率化
+  config.include FactoryBot::Syntax::Methods
+
 end
