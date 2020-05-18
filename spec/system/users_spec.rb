66a9f269e97fc5e7de953d353d2938585a7218a2
m-y-g-daisuke <disk.myg@gmail.com>
2020-05-18 16:23:29 +0900

    userのsystem specの実装
    ログインから編集まで

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,13 +2,18 @@ RSpec.describe 'Users', type: :system do
   let!(:user){create(:user)}
   
   describe "User" do
-    it 'log_in' do
+    before  do
       visit user_session_path
       fill_in "user_email", with: "#{user.email}"
       fill_in "user_password", with: "#{user.password}"
       click_button 'Log in'
+    end
+    
+      it "login now" do
       expect(current_path).to eq root_path
+      end
 
+      it "edit user" do
       click_on "#{user.name}"
       click_link 'ユーザー編集', href: edit_user_path(user)
 
